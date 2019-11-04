282bc905ef7b32ab0fce102ec33dfec05fb80960
Mica <micamurphy42@gmail.com>
2019-11-04 15:54:46 +0100

    II - Fin partie 4 TP1

--- II/TP/1-grapher/grapher.src/grapher/src/grapher/ui/Main.java
+++ II/TP/1-grapher/grapher.src/grapher/src/grapher/ui/Main.java
@@ -1,21 +1,34 @@
 package grapher.ui;
 
+import grapher.fc.Function;
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-
-
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	public void start(Stage stage) {
-		BorderPane root = new BorderPane();
-		root.setCenter(new GrapherCanvas(getParameters()));
-		
+		GrapherCanvas c = new GrapherCanvas(getParameters());
+		SplitPane sp = new SplitPane();
+
+		ObservableList<Function> f = FXCollections.observableArrayList();
+		f.addAll(c.functions);
+		ListView<Function> lv = new ListView<Function>(f);
+		BorderPane root = new BorderPane(lv);
+		root.setCenter(lv);
+
+		sp.getItems().add(root);
+		sp.getItems().add(c);
+
 		stage.setTitle("grapher");
-		stage.setScene(new Scene(root));
+		stage.setScene(new Scene(sp));
 		stage.show();
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
