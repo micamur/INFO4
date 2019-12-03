4e1d7c5761b436fa6e128b3555030d5134626a64
Mica <micamurphy42@gmail.com>
2019-12-03 13:34:04 +0100

    II - TP2 fin

--- II/TP/downloader.src/downloader/src/downloader/ui/DownloaderView.java
+++ II/TP/downloader.src/downloader/src/downloader/ui/DownloaderView.java
@@ -2,7 +2,10 @@ package downloader.ui;
 
 import downloader.fc.Downloader;
 import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class DownloaderView {
@@ -11,16 +14,35 @@ public class DownloaderView {
 
 	public DownloaderView(String url, VBox vb) {
 		try {
+			HBox hb = new HBox();
+			
+			Button playPause = new Button(">");
+			Button delete = new Button("X");
+			playPause.setPrefWidth(50);
+			delete.setPrefWidth(50);
+			
 			downloader = new Downloader(url);
 
+			// TODO : la barre de progression n'apparaît pas
+			
 			pb = new ProgressBar(0);
+			pb.setMinWidth(vb.getWidth() - 100);
+			pb.setMaxWidth(vb.getWidth() - 100);
 			pb.setPrefWidth(Double.MAX_VALUE);
+			
 			downloader.progressProperty().addListener((obs, o, n) -> {
 				System.out.print(".");
 				System.out.flush();
 				Platform.runLater(() -> pb.setProgress(downloader.progressProperty().get()));
 			});
-			vb.getChildren().add(pb);
+			
+			vb.getChildren().add(new Label(downloader.toString()));
+			
+			hb.getChildren().add(pb);
+			hb.getChildren().add(playPause);
+			hb.getChildren().add(delete);
+			
+			vb.getChildren().add(hb);
 
 			new Thread(downloader).start();
 		} catch (RuntimeException e) {
