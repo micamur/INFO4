419484d6ffc68116723ac1319df1528071e3160b
Mica <micamurphy42@gmail.com>
2020-02-17 09:59:11 +0100

    AR - TP2 NIO babysteps

---
+++ AR/TP/ricm.nio.babysteps/babysteps/ricm/distsys/nio/babystep3/NioServer.java
@@ -0,0 +1,174 @@
+package ricm.distsys.nio.babystep3;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * NIO elementary server RICM4 TP F. Boyer
+ */
+
+public class NioServer {
+
+	public static int DEFAULT_SERVER_PORT = 8888;
+
+	// The channel used to accept connections from server-side
+	private ServerSocketChannel ssc;
+	private SelectionKey sscKey;
+
+	// Unblocking selector
+	private Selector selector;
+
+	// Automata hashmap
+	private HashMap<SelectionKey,Writer> hWriter;
+	private HashMap<SelectionKey,Reader> hReader;
+
+	/**
+	 * NIO server initialization
+	 * 
+	 * @param the host address and port of the server
+	 * @throws IOException
+	 */
+	public NioServer(int port) throws IOException {
+
+		// create a new selector
+		selector = SelectorProvider.provider().openSelector();
+
+		// create a new non-blocking server socket channel
+		ssc = ServerSocketChannel.open();
+		ssc.configureBlocking(false);
+
+		// bind the server socket to the given address and port
+		InetAddress hostAddress;
+		hostAddress = InetAddress.getByName("localhost");
+		InetSocketAddress isa = new InetSocketAddress(hostAddress, port);
+		ssc.socket().bind(isa);
+
+		// be notified when connection requests arrive
+		sscKey = ssc.register(selector, SelectionKey.OP_ACCEPT);
+		
+		// create new automata hashmap
+		hWriter = new HashMap<SelectionKey,Writer>();
+		hReader = new HashMap<SelectionKey,Reader>();
+	}
+
+	/**
+	 * NIO mainloop Wait for selected events on registered channels Selected events
+	 * for a given channel may be ACCEPT, CONNECT, READ, WRITE Selected events for a
+	 * given channel may change over time
+	 */
+	public void loop() throws IOException {
+		System.out.println("NioServer running");
+		while (true) {
+			selector.select();
+
+			Iterator<?> selectedKeys = this.selector.selectedKeys().iterator();
+
+			// Goes through all I/O ready clients
+			while (selectedKeys.hasNext()) {
+				SelectionKey key = (SelectionKey) selectedKeys.next();
+				selectedKeys.remove();
+				if (key.isValid() && key.isAcceptable())
+					handleAccept(key);
+				if (key.isValid() && key.isReadable())
+					handleRead(key);
+				if (key.isValid() && key.isWritable())
+					handleWrite(key);
+				if (key.isValid() && key.isConnectable())
+					handleConnect(key);
+			}
+		}
+	}
+
+	/**
+	 * Accept a connection and make it non-blocking
+	 * 
+	 * @param the key of the channel on which a connection is requested
+	 */
+	private void handleAccept(SelectionKey key) throws IOException {
+		assert (this.sscKey == key);
+		assert (ssc == key.channel());
+		
+		// get the socket channel for the client who sent something
+		SocketChannel sc;
+
+		// do the actual accept on the server-socket channel
+		sc = ssc.accept();
+		sc.configureBlocking(false);
+
+		// register the read interest for the new socket channel
+		// in order to know when there are bytes to read
+		SelectionKey keyClient = sc.register(this.selector, SelectionKey.OP_READ);
+		
+		// create automata for this client
+		hWriter.put(keyClient, new Writer(keyClient));
+		hReader.put(keyClient, new Reader(keyClient));
+	}
+
+	/**
+	 * Handle a connect, this should never happen
+	 * 
+	 * @param the key of the channel on which a connection is requested
+	 * @throws Error since this should never happen
+	 */
+	private void handleConnect(SelectionKey key) throws IOException {
+		throw new Error("Unexpected connect");
+	}
+
+	/**
+	 * Handle incoming data event
+	 * 
+	 * @param the key of the channel on which the incoming data waits to be received
+	 */
+	private void handleRead(SelectionKey key) throws IOException {
+		assert (sscKey != key);
+		assert (ssc != key.channel());
+
+		// Let's read the message
+		String data = hReader.get(key).nextState();
+		
+		if (data != null) {
+			System.out.println("NioServer3 received from " + key.channel().hashCode() + ": " + data);
+			
+			// echo back the same message to the client
+			hWriter.get(key).send(data.getBytes(Charset.forName("UTF-8")));					
+		}
+	}
+
+	/**
+	 * Handle outgoing data event
+	 * 
+	 * @param the key of the channel on which data can be sent
+	 */
+	private void handleWrite(SelectionKey key) throws IOException {
+		assert (sscKey != key);
+		assert (ssc != key.channel());
+
+		// get back the buffer that we must send
+		hWriter.get(key).nextState();
+	}
+
+	public static void main(String args[]) throws IOException {
+		int serverPort = DEFAULT_SERVER_PORT;
+		String arg;
+
+		for (int i = 0; i < args.length; i++) {
+			arg = args[i];
+			if (arg.equals("-p")) {
+				serverPort = Integer.valueOf(args[++i]).intValue();
+			}
+		}
+		NioServer ns;
+		ns = new NioServer(serverPort);
+		ns.loop();
+	}
+
+}
---
+++ AR/TP/ricm.nio.babysteps/babysteps/ricm/distsys/nio/babystep3/Writer.java
@@ -0,0 +1,70 @@
+package ricm.distsys.nio.babystep3;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
+
+public class Writer {
+	private enum State {IDLE, WAIT_LEN, WAIT_MSG};
+	private State currentState;
+	private ByteBuffer msgBuffer;
+	private ByteBuffer msgLenBuffer;
+	private SocketChannel sc;
+	private int msgLen;
+	private LinkedList<ByteBuffer> msgWaiting;
+	private SelectionKey key;
+	
+	public Writer(SelectionKey key) {
+		this.key = key;
+		this.currentState = State.IDLE;
+		this.sc = (SocketChannel) key.channel();
+		this.msgLenBuffer = ByteBuffer.allocate(4);
+		this.msgWaiting = new LinkedList<ByteBuffer>();
+	}
+	
+	public void nextState() throws IOException {
+		switch (currentState) {
+		case IDLE:
+			currentState = State.WAIT_LEN;
+			msgBuffer = msgWaiting.removeFirst();
+			msgLen = msgBuffer.remaining();
+			msgLenBuffer.rewind();
+			msgLenBuffer.putInt(msgLen);
+			msgLenBuffer.rewind();
+		case WAIT_LEN:
+			sc.write(msgLenBuffer);
+			if (msgLenBuffer.remaining() == 0) {
+				currentState = State.WAIT_MSG;
+			}
+		case WAIT_MSG:
+			sc.write(msgBuffer);
+			if (msgBuffer.remaining() == 0) {
+				currentState = State.IDLE;
+				String msg = new String(msgBuffer.array(), "UTF-8");
+				System.out.println("Write: " + msg.length());
+				key.interestOps(SelectionKey.OP_READ);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	/**
+	 * Send the given data
+	 * 
+	 * @param data: the byte array that should be sent
+	 */
+	public void send(byte[] newMsg) {
+		if (newMsg.length == 0)
+			return;
+		
+		msgWaiting.add(ByteBuffer.wrap(newMsg));
+		
+		// register a write interests to know when there is room to write
+		// in the socket channel.
+		key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+	}
+}
