0d6b740920bfdd7203be4df90f89b7a3952acd74
Mica <micamurphy42@gmail.com>
2020-02-17 10:01:03 +0100

    SI - TP4

--- SI/TP/4/shader/fragment.glsl
+++ SI/TP/4/shader/fragment.glsl
@@ -6,8 +6,8 @@ in vec4 my_color;
 out vec4 frag_color;
 
 // Fonction appellee pour chaque fragment
-void main()
-{
+void main() {
+
   // Affectation de la couleur du fragment
-  frag_color = vec4(my_color.rgb ,1.0);
+  frag_color = my_color;
 }
--- SI/TP/4/shader/vertex.glsl
+++ SI/TP/4/shader/vertex.glsl
@@ -2,10 +2,9 @@
 #version 330
 
 // Donnees d'entree
-//layout(location=0) in vec3 in_position;
 in vec3 in_position;
 in vec3 in_color;
-
+in vec3 in_normal;
 
 // Donnees de sortie
 out vec4 my_color;
@@ -16,12 +15,10 @@ uniform mat4 ViewMatrix;
 uniform mat4 ProjectionMatrix;
 
 // Fonction appellee pour chaque sommet
-void main()
-{
+void main() {
   // Affectation de la position du sommet
   // gl_Position est definit par defaut dan GLSL
   gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(in_position, 1.0);
 
-  //my_color = vec4((in_position + vec3(1,1,1))*0.5, 1.0);
-  my_color = vec4(in_color,1.0);
+  my_color = vec4(in_normal*0.5+0.5,1.0);
 }
